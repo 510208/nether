9a13858ddef1cd2b11de11e3cec15d20f903bf40
510208 <xux510208@gmail.com>
2023-12-03 12:32:14 +0800

    Update Latest

--- main.py
+++ main.py
@@ -37,6 +37,13 @@ async def ip(ctx: discord.ApplicationContext):
     # 判斷伺服器是否開啟
     if config["java-server"]["opened"]:
         jeId = config["java-server"]["mc-server-ip"]
+        jeVer = config["java-server"]["mc-server-version"]
+        if config["java-server"]["mc-server-port"] == "":
+            jePort = "19132"
+        else:
+            jePort = config["bedrock-server"]["mc-server-port"]
+    
+        jeId = config["java-server"]["mc-server-ip"] + ":" + str(jePort)
     else:
         jeId = "伺服器未開啟"
     
@@ -44,30 +51,63 @@ async def ip(ctx: discord.ApplicationContext):
         # 是否為Realm伺服器
         if config["bedrock-server"]["mc-server-is-realm"]:
             beId = f"Realm伺服器，加入連結為{config['bedrock-server']['mc-server-realm-uri']}"
+            beVer = "目前的最新版本"
         else:
             beId = config["bedrock-server"]["mc-server-ip"]
+            if config["bedrock-server"]["mc-server-latested"]:
+                beVer = "目前的最新版本"
+            else:
+                beVer = config["bedrock-server"]["mc-server-version"]
+        if config["bedrock-server"]["mc-server-port"] == "":
+            bePort = "19132"
+        else:
+            bePort = config["bedrock-server"]["mc-server-port"]
     else:
         beId = "伺服器未開啟"
     
     # 創建一個 Embed 物件
     embed = discord.Embed(title="伺服器資訊", color=0x00ff00)
     embed.add_field(name="Java 伺服器", value=jeId, inline=False)
+    embed.add_field(name="Java 版本", value=jeVer, inline=False)
     embed.add_field(name="Bedrock 伺服器", value=beId, inline=False)
+    embed.add_field(name="Bedrock 端口", value=bePort, inline=False)
+    embed.add_field(name="Bedrock 版本", value=beVer, inline=False)
     embed.set_footer(text="機器人由 SamHacker 設計", icon_url=SAMHEADERURL)
 
     # 發送 Embed 訊息
     await ctx.respond(embed=embed)
 
+# ======================
+# 取得伺服器版本(Beta)
+# ======================
 @bot.slash_command(name="version", description="顯示伺服器版本")
 async def version(ctx: discord.ApplicationContext):
-    # 取得Config中的版本信息
-    jeVer = config["java-server"]["mc-server-version"]
-    beVer = config["bedrock-server"]["mc-server-version"]
+    # 判斷伺服器是否開啟
+    if config["java-server"]["opened"]:
+        jeVer = config["java-server"]["mc-server-version"]
+    else:
+        jeVer = "伺服器未開啟"
+
+    if config["bedrock-server"]["opened"]:
+        # 是否為Realm伺服器
+        if config["bedrock-server"]["mc-server-is-realm"]:
+            beVer = "目前的最新版本"
+        else:
+            if config["bedrock-server"]["mc-server-latested"]:
+                beVer = "目前的最新版本"
+            else:
+                beVer = config["bedrock-server"]["mc-server-version"]
+    else:
+        beVer = "伺服器未開啟"
+    
     # 創建一個 Embed 物件
     embed = discord.Embed(title="伺服器版本", color=0x00ff00)
     embed.add_field(name="Java 版本", value=jeVer, inline=False)
     embed.add_field(name="Bedrock 伺服器", value=beVer, inline=False)
     embed.set_footer(text="機器人由 SamHacker 設計", icon_url=SAMHEADERURL)
+    
+    # 發送 Embed 訊息
+    await ctx.respond(embed=embed)
 
 
 @bot.slash_command(name="say", description="借刀殺人好方法")
@@ -76,9 +116,13 @@ async def global_command(
 ):  # Takes one integer parameter
     await ctx.respond(message)
 
-@bot.slash_command(name="ping", description="顯示機器人延遲")
+@bot.slash_command(name="alive", description="機器人的生命到頭沒")
+async def alive(ctx: discord.ApplicationContext):
+    await ctx.respond("我是機器人，不會死")
+
+@bot.slash_command(name="乒", description="顯示機器人延遲")
 async def ping(ctx: discord.ApplicationContext):
-    await ctx.respond(f"{round(bot.latency * 1000)}ms(還真是九千年前的機器人啊))")
+    await ctx.respond(f"乓！{round(bot.latency * 1000)}ms")
 
 # 系統訊息
 
@@ -97,6 +141,10 @@ async def on_message(message):
     # 如果訊息提及了 bot，則回覆 "有何貴事"
     if bot.user in message.mentions:
         await message.reply("有何貴事")
+    
+    # 如果訊息內容包含 "活著嗎"，則回覆 "我是機器人，不會死"
+    if "活著嗎" in message.content:
+        await message.reply("我是機器人，不會死")
 
 # To learn how to add descriptions and choices to options, check slash_options.py
 bot.run(TOKEN)
